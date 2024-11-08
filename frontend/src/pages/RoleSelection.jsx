d786b4eea3767fd46973f29d8f03789111543005
Aryan Raghuvanshi <124778245+AryanRaghuvanshi09@users.noreply.github.com>
2024-11-08 13:27:24 +0530

    Update RoleSelection.jsx

--- frontend/src/pages/RoleSelection.jsx
+++ frontend/src/pages/RoleSelection.jsx
@@ -1,21 +1,33 @@
-// RoleSelection.jsx
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../styles/RoleSelection.css';
 
 const RoleSelection = () => {
-  const navigate = useNavigate(); // Use useNavigate instead of useHistory
+  const navigate = useNavigate();
 
   const handleRoleSelection = (role) => {
-    navigate(`/login/${role}`); // Navigate to login with the selected role
+    navigate(`/login/${role}`);
   };
 
   return (
-    <div>
-      <h2>Select Your Role</h2>
-      <button onClick={() => handleRoleSelection("admin")}>Admin</button>
-      <button onClick={() => handleRoleSelection("salesperson")}>
-        Salesperson
-      </button>
+    <div className="role-selection">
+      <nav className="navbar">
+        {/* Your company logo or name here */}
+        <h1 className="navbar-title">Swastik Traders</h1>
+        <h4>(Shastripuram Agra)</h4>
+      </nav>
+
+      <main className="main-content">
+        <div className="role-card">
+          <h2>Select Your Role</h2>
+          <button className="role-button" onClick={() => handleRoleSelection('admin')}>
+            <h4>Admin</h4>
+          </button>
+          <button className="role-button" onClick={() => handleRoleSelection('salesperson')}>
+          <h4>Salesperson</h4>
+          </button>
+        </div>
+      </main>
     </div>
   );
 };
